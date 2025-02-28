f16c9ecc0cbebfb89f9542391157ab5419973811
Shakil <sakildevmern@gmail.com>
2025-02-28 23:55:35 +0600

    2/28/2025

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,7 @@
 import CardStacks from "./CardStacks";
 import AnimatedGradientText from "./AnimatedGradientText";
 import { motion } from "framer-motion";
-import { useEffect } from "react";
+import { Fragment, useEffect } from "react";
 import { fetchAdminData } from "../redux/api/visitor/fetchAdminData.js";
 import { useDispatch, useSelector } from "react-redux";
 import Loader from "./Loader.jsx";
@@ -11,17 +11,34 @@ const Banner = () => {
   const { data, loading, error } = useSelector(
     (state) => state.adminDataReducer
   );
+  // handle re data fetching 
+  function handleRetry() {
+    return dispatch(fetchAdminData());
+  }
 
   // call admin API to get the banner data
   useEffect(() => {
     dispatch(fetchAdminData());
   }, [dispatch]);
   //
-  console.log(data, loading, error);
+  console.log(data?.about);
   if (loading) {
     return <Loader></Loader>;
   }
-  if (error) return <p style={{ color: "red" }}>{error}</p>;
+  if (error) {
+    return (
+      <div className="text-center">
+        <h1>Oh Uh</h1>
+        <p>{error}</p>
+        <button
+          className="px-2 py-1 bg-blue-400 rounded-md text-white font-primary cursor-pointer mt-2"
+          onClick={handleRetry}
+        >
+          Re Tray
+        </button>
+      </div>
+    );
+  }
   //
   return (
     <section className="flex justify-between items-center h-screen py-10 gap-10 container mx-auto">
@@ -30,10 +47,10 @@ const Banner = () => {
         animate={{ opacity: 1, x: 0 }} // Animate to visible
         transition={{ duration: 1, delay: 0.2 }} // Delay for smooth effect
       >
-        {/* <AnimatedGradientText
-          text={data?.data.about}
+        <AnimatedGradientText
+          text={data?.about}
           className="font-bold text-7xl"
-        /> */}
+        />
       </motion.div>
       <motion.div
         initial={{ opacity: 0, x: 10 }} // Start hidden, move from right
@@ -41,7 +58,7 @@ const Banner = () => {
         transition={{ duration: 1, delay: 0.2 }} // Delay for smooth effect
         className=""
       >
-        {/* <CardStacks images={data?.data.images} /> */}
+        <CardStacks images={data?.images} />
       </motion.div>
     </section>
   );
